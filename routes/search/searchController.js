26098fba8deadd4c3a2342a9890d8b4640d909e4
Miley Dawn <mileydawn@huongs-mbp.myfiosgateway.com>
2021-04-29 15:27:46 -0400

    change port 4000 to 3001

--- routes/search/searchController.js
+++ routes/search/searchController.js
@@ -11,18 +11,18 @@ module.exports = {
         // console.log('=====================================location', req.query.location)
 
         try {
-            // console.log('==================', req.query)
-            const parameters = {
-                key: process.env.GOOGLE_API_KEY,
-                location: req.query.location,
-                // location: '40.67,-73.95',
-                keyword: req.query.keyword,
-                radius: '2000',
-            }
-            const baseUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
-            let response = await axios.get(baseUrl + new URLSearchParams(parameters))
-            res.status(200).send(response.data)
-            // res.status(200).send(sushiPlaces)
+            // // console.log('==================', req.query)
+            // const parameters = {
+            //     key: process.env.GOOGLE_API_KEY,
+            //     location: req.query.location,
+            //     // location: '40.67,-73.95',
+            //     keyword: req.query.keyword,
+            //     radius: '2000',
+            // }
+            // const baseUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
+            // let response = await axios.get(baseUrl + new URLSearchParams(parameters))
+            // res.status(200).send(response.data)
+            res.status(200).send(sushiPlaces)
         }
         catch (err) {
             res.status(400).send(err)
