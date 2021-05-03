cff81785734a8c0a7a0cd1d5f5397fb384757b26
Miley Dawn <mileydawn@huongs-mbp.myfiosgateway.com>
2021-05-03 09:22:28 -0400

    getPlaceDetail works again

--- routes/search/searchController.js
+++ routes/search/searchController.js
@@ -11,18 +11,18 @@ module.exports = {
         // console.log('=====================================location', req.query.location)
 
         try {
-            // // console.log('==================', req.query)
-            // const parameters = {
-            //     key: process.env.GOOGLE_API_KEY,
-            //     location: req.query.location,
-            //     // location: '40.67,-73.95',
-            //     keyword: req.query.keyword,
-            //     radius: '2000',
-            // }
-            // const baseUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
-            // let response = await axios.get(baseUrl + new URLSearchParams(parameters))
-            // res.status(200).send(response.data)
-            res.status(200).send(sushiPlaces)
+            // console.log('==================', req.query)
+            const parameters = {
+                key: process.env.GOOGLE_API_KEY,
+                location: req.query.location,
+                // location: '40.67,-73.95',
+                keyword: req.query.keyword,
+                radius: '2000',
+            }
+            const baseUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
+            let response = await axios.get(baseUrl + new URLSearchParams(parameters))
+            res.status(200).send(response.data)
+            // res.status(200).send(sushiPlaces)
         }
         catch (err) {
             res.status(400).send(err)
@@ -31,15 +31,15 @@ module.exports = {
 
     placeDetail: async (req, res) => {
         try {
-            // const baseUrl = 'https://maps.googleapis.com/maps/api/place/details/json?'
-            // const parameters = {
-            //     key: process.env.GOOGLE_API_KEY,
-            //     place_id: req.params.id,
-            //     fields: 'business_status,name,place_id,geometry,rating,price_level,formatted_phone_number,website,url,vicinity,types,reviews,opening_hours'
-            // }
-            // let response = await axios.get(baseUrl + new URLSearchParams(parameters))
-            // res.status(200).send(response.data)
-            res.status(200).send(sushiLounge)
+            const baseUrl = 'https://maps.googleapis.com/maps/api/place/details/json?'
+            const parameters = {
+                key: process.env.GOOGLE_API_KEY,
+                place_id: req.params.id,
+                fields: 'business_status,name,place_id,geometry,rating,price_level,formatted_phone_number,website,url,vicinity,types,reviews,opening_hours'
+            }
+            let response = await axios.get(baseUrl + new URLSearchParams(parameters))
+            res.status(200).send(response.data)
+            // res.status(200).send(sushiLounge)
         } catch (err) {
             res.status(400).send(err)
         }
